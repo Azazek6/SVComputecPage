8f9c8ef7ea68aa4fa9c97e747de089c03eacc2e5
Azazek6 <naturaespinola12@gmail.com>
2023-05-21 22:18:56 -0500

    add: files

--- src/helpers/configuration.js
+++ src/helpers/configuration.js
@@ -1,4 +1,4 @@
-const uriWeb = "backendcomputec-production.up.railway.app/api";
+const uriWeb = "https://backendcomputec-production.up.railway.app/api";
 const uriLocal = "http://localhost:5000/api";
 
-export const connectURL = uriWeb;
+export const connectURL = uriLocal;
